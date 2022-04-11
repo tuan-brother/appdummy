b5100eb6f1949b710bbaf91093cd722e5b92cb33
tuan-brother <phungtuananhxy@gmail.com>
2022-04-11 16:52:39 +0700

    update_welcome_ui

--- app/src/main/java/com/example/app_ban_hang_tot_nghiep/WelcomeActivity.java
+++ app/src/main/java/com/example/app_ban_hang_tot_nghiep/WelcomeActivity.java
@@ -5,6 +5,7 @@ import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.app_ban_hang_tot_nghiep.databinding.ActivityWelcomeBinding;
 import com.example.app_ban_hang_tot_nghiep.fragment.StartFragment;
@@ -17,12 +18,18 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mWelcomeBinding = DataBindingUtil.setContentView(this, R.layout.activity_welcome);
-        gotoMain();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                gotoMain();
+            }
+        }, 3000);
     }
 
     public void gotoMain() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+        finish();
     }
 
     public void gotoStartFragment() {
