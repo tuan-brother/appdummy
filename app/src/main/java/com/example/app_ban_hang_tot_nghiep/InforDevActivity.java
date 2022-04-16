46ea29eac58229ce0b5ec619c40f0b27c73c9d40
tuan-brother <phungtuananhxy@gmail.com>
2022-04-17 00:34:26 +0700

    update icon and text color

--- app/src/main/java/com/example/app_ban_hang_tot_nghiep/InforDevActivity.java
+++ app/src/main/java/com/example/app_ban_hang_tot_nghiep/InforDevActivity.java
@@ -2,7 +2,10 @@ package com.example.app_ban_hang_tot_nghiep;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 public class InforDevActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class InforDevActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_infor_dev);
+        this.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        this.getWindow().setStatusBarColor(Color.TRANSPARENT);
     }
 }
